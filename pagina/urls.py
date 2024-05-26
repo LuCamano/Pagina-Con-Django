57061961b8a330ec1c18b76d39fbe287930deb00
LuCamano <lu.camano@duocuc.cl>
2024-05-26 16:55:04 -0400

    Vistas de login y logout implementadas

--- pagina/urls.py
+++ pagina/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('admin/', views.admin, name='admin'),
+    path('cart/', views.cart, name='cart'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('producto/', views.producto, name='producto'),
+    path('registrarse/', views.registrarse, name='registrarse'),
+    path('login/', views.loginV, name='login'),
+    path('logout/', views.logoutV, name='logout'),
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -1,5 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+def admin(request):
+    return render(request, 'admin.html', {})
+
+def cart(request):
+    return render(request, 'cart.html', {})
+
+@login_required
+def perfil(request):
+    return render(request, 'perfil.html', {})
+
+def producto(request):
+    return render(request, 'producto.html', {})
+
+def registrarse(request):
+    return render(request, 'registrarse.html', {})
+
+def loginV(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+    else:
+        return redirect('login')
+
+def logoutV(request):
+    logout(request)
+    return redirect('index')
